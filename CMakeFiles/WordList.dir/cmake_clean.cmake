48bbaa6c830607f1648499a72d5da81767c159be
Wynne Nicholas <wynne.nicholas@outlook.com>
2022-03-03 18:40:41 -0600

    initial commit

---
+++ CMakeFiles/WordList.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WordList.dir/nicholas_wynne_hw4_main.c.o"
+  "CMakeFiles/WordList.dir/wordList.c.o"
+  "WordList"
+  "WordList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WordList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/wordList.h.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libwordList.h.a"
+)
